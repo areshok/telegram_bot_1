8ad566a7aa6200dda4d30d841afa06658055ef44
Арсений Турлапов <arsturlapov@yandex.ru>
2025-04-05 23:15:47 +0400

    product models add verbose_name in fields

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,21 +1,29 @@
 from django.db import models
-from .validation import SingleCharacterValidator, MinCharacterValidator
-from django.core.validators import URLValidator
+from .validation import MinCharacterValidator
+
+
 
 class Product(models.Model):
+    "Таблица товаров"
+
     name = models.CharField(
         max_length=250,
         validators=[MinCharacterValidator(2),],
+        verbose_name='название товара'
     )
     description = models.TextField(
         validators=[MinCharacterValidator(2)],
+        verbose_name='описание товара',
     )
     qrcode = models.ImageField(
         upload_to='qrcode/',
         blank=True,
         null=True,
     )
-    date_create = models.DateTimeField(auto_now_add=True)
+    date_create = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='дата создания'
+        )
 
     class Meta:
         ordering = ['-id']
@@ -23,10 +31,14 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class Marketplace(models.Model):
+    "Таблица маркетплейсов"
+
     name = models.CharField(
         max_length=100,
         validators=[MinCharacterValidator(2)],
+        verbose_name='название маркетплейса',
         )
 
     def __str__(self):
@@ -34,6 +46,10 @@ class Marketplace(models.Model):
 
 
 class ProductMarketplace(models.Model):
+    "Таблица ссылок товаров на маркетлейсах"
+
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     marketplace_id = models.ForeignKey(Marketplace, on_delete=models.CASCADE)
-    url = models.URLField()
+    url = models.URLField(
+        verbose_name='ссылка на маркетплейс',
+    )
